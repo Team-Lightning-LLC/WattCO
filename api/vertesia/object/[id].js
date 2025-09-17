51c6b88ff0e9d6b1a3ef0d41af0416b0a8fab9e1
Team-Lightning-LLC <lehmanachris@gmail.com>
2025-09-17 14:26:01 -0400

    Update [id].js

--- api/vertesia/object/[id].js
+++ api/vertesia/object/[id].js
@@ -1,1 +1,9 @@
+import { base, auth } from './_util.js';
+export default async (req) => {
+  const id = req.url.split('object-')[1];
+  const r = await fetch(`${base}/objects/${id}`, {
+    method: req.method, headers: auth
+  });
+  return new Response(await r.text(), { status:r.status, headers:{'Content-Type':'application/json'} });
+};
 
